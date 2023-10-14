220889ae746bb88e3baf10aa6a847ed4ceabea8a
Manali Renavikar <manalirenavikar@gmail.com>
2023-10-14 20:47:19 +0100

    Add project files.

---
+++ Burp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Burp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rem = 0;
+            int Remainder(int x, int y)
+            {
+                rem = x % y;
+                return rem;
+            }
+            Remainder(3, 4);
+            Console.WriteLine("Remainder is " + rem);
+            Console.ReadLine();
+            
+        }
+    }
+}
